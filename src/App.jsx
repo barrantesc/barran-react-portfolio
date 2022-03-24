8b7e88ac68e276949d2cc20eca9a6542ac19c263
playground <connie.s.barrantes@gmail.com>
2022-03-23 22:21:42 -0400

    added projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Intro from "./components/Intro/Intro.js"
 import About from "./components/About/About.js"
+import ProductList from "./components/PortfolioList/PortfolioList.js"
 
 const App = () => {
   return (
   <div>
     <Intro/>
     <About/>
+    <ProductList/>
   </div>
 )
 };
---
+++ src/data.js
@@ -0,0 +1,39 @@
+import sheetShow from './img/SheetShow-img.png'
+import weatherApp from './img/WeatherApp-img.png'
+import purrfectFriend from './img/PurrfectFriend-img.gif'
+import noteTaker from './img/NoteTaker-img.png'
+import simplePlanner from './img/Planner-img.png'
+import timedQuiz from './img/Quiz-img.png'
+
+export const portfolio = [
+    {
+        id: 1,
+        img: sheetShow,
+        link: "https://sheet-show.herokuapp.com/",
+    },
+    {
+        id: 2,
+        img: weatherApp,
+        link: "https://barrantesc.github.io/My_Cool-WeatherApp/",
+    },
+    {
+        id: 3,
+        img: purrfectFriend,
+        link: "https://erikplachta.github.io/Purrfect-Friend/",
+    },
+    {
+        id: 4,
+        img: noteTaker,
+        link: "https://sheltered-taiga-87372.herokuapp.com/",
+    },
+    {
+        id: 5,
+        img: simplePlanner,
+        link: "https://barrantesc.github.io/Simple-Planner/",
+    },
+    {
+        id: 6,
+        img: timedQuiz,
+        link: "https://barrantesc.github.io/Timed-Quiz/",
+    },
+];
